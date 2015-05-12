27bdff97747cb0d3cf898881a820d9d60559c316
Zack <Zack@bitinerary.com>
2015-05-12 12:55:05 -0500

    rm redundant progfiles folder
    Just keep testing to a different folder than GitHub Main.

--- ProgFilesWOcfg's/tweaking/files/remove_symbolic_links_from_windows_defender_folder.bat
+++
@@ -1,7 +0,0 @@
-FOR /R "%ProgramFiles(x86)%\Windows Defender\" %%G in (.) DO (fsutil reparsepoint delete "%%G")
-
-FOR /R "%ProgramFiles(x86)%\Windows Defender\" %%G in (*.*) DO (fsutil reparsepoint delete "%%G")
-
-FOR /R "%ProgramFiles%\Windows Defender\" %%G in (.) DO (fsutil reparsepoint delete "%%G")
-
-FOR /R "%ProgramFiles%\Windows Defender\" %%G in (*.*) DO (fsutil reparsepoint delete "%%G")
