6176bc8d18d7c7e0192e2dd0025d21cdac04e7a1
Zack <izaac.corbett@gmail.com>
2015-05-14 20:39:06 -0500

    root directory accepts spaces.
    Forgot to surround Windows commands with quotes. inside subprocess.Popen(' " " ') function.

--- BLDZR.py
+++ BLDZR.py
@@ -45,17 +45,17 @@ class DiagnosticRepairProgs(tk.Frame):
 
 		BackButton0 = tk.Button(self, width =20, height = 5, text="Go Back", command=lambda: controller.show_frame(StartPage))
 
-		PageOneButton1 = tk.Button(self, width=20, height=5, text="Function1", command=lambda: subprocess.Popen('', shell=True))
-		PageOneButton2 = tk.Button(self, width=20, height=5, text="Function2", command=lambda: subprocess.Popen('', shell=True))
-		PageOneButton3 = tk.Button(self, width=20, height=5, text="Function3", command=lambda: subprocess.Popen('', shell=True))
+		PageOneButton1 = tk.Button(self, width=20, height=5, text="Function1", command=lambda: subprocess.Popen('""', shell=True))
+		PageOneButton2 = tk.Button(self, width=20, height=5, text="Function2", command=lambda: subprocess.Popen('""', shell=True))
+		PageOneButton3 = tk.Button(self, width=20, height=5, text="Function3", command=lambda: subprocess.Popen('""', shell=True))
 
-		PageOneButton4 = tk.Button(self, width=20, height=5, text="Function4", command=lambda: subprocess.Popen('', shell=True))
-		PageOneButton5 = tk.Button(self, width=20, height=5, text="Function5", command=lambda: subprocess.Popen('', shell=True))
-		PageOneButton6 = tk.Button(self, width=20, height=5, text="Function6", command=lambda: subprocess.Popen('', shell=True))
+		PageOneButton4 = tk.Button(self, width=20, height=5, text="Function4", command=lambda: subprocess.Popen('""', shell=True))
+		PageOneButton5 = tk.Button(self, width=20, height=5, text="Function5", command=lambda: subprocess.Popen('""', shell=True))
+		PageOneButton6 = tk.Button(self, width=20, height=5, text="Function6", command=lambda: subprocess.Popen('""', shell=True))
 
-		PageOneButton7 = tk.Button(self, width=20, height=5, text="Function7", command=lambda: subprocess.Popen('', shell=True))
-		PageOneButton8 = tk.Button(self, width=20, height=5, text="Function8", command=lambda: subprocess.Popen('', shell=True))
-		PageOneButton9 = tk.Button(self, width=20, height=5, text="Function9", command=lambda: subprocess.Popen('', shell=True))
+		PageOneButton7 = tk.Button(self, width=20, height=5, text="Function7", command=lambda: subprocess.Popen('""', shell=True))
+		PageOneButton8 = tk.Button(self, width=20, height=5, text="Function8", command=lambda: subprocess.Popen('""', shell=True))
+		PageOneButton9 = tk.Button(self, width=20, height=5, text="Function9", command=lambda: subprocess.Popen('""', shell=True))
 
 		BackButton0.grid(row=3, column=2, padx=10, pady=10)
 
@@ -80,17 +80,17 @@ class AutOptimizer(tk.Frame):
 		
 		BackButton0 = tk.Button(self, width =20, height = 5, text="Go Back", command=lambda: controller.show_frame(StartPage))
 
-		PageTwoButton1 = tk.Button(self, width=20, height=5, text="Startup Porgams", command=lambda: subprocess.Popen('%CD%/ProgFiles/whatinstartup/whatinstartup.exe && taskmgr /0 /startup', shell=True))
-		PageTwoButton2 = tk.Button(self, width=20, height=5, text="Scheduled Tasks", command=lambda: subprocess.Popen('taskschd.msc', shell=True))
-		PageTwoButton3 = tk.Button(self, width=20, height=5, text="Services", command=lambda: subprocess.Popen('services.msc', shell=True))
+		PageTwoButton1 = tk.Button(self, width=20, height=5, text="Startup Porgams", command=lambda: subprocess.Popen('"%CD%/ProgFiles/whatinstartup/whatinstartup.exe && taskmgr /0 /startup"', shell=True))
+		PageTwoButton2 = tk.Button(self, width=20, height=5, text="Scheduled Tasks", command=lambda: subprocess.Popen('"taskschd.msc"', shell=True))
+		PageTwoButton3 = tk.Button(self, width=20, height=5, text="Services", command=lambda: subprocess.Popen('"services.msc"', shell=True))
 
-		PageTwoButton4 = tk.Button(self, width=20, height=5, text="Bulk Uninstall", command=lambda: subprocess.Popen('%CD%/ProgFiles/myuninst/myuninst.exe', shell=True))
-		PageTwoButton5 = tk.Button(self, width=20, height=5, text="Windows Directory\nStatistics", command=lambda: subprocess.Popen('%CD%/ProgFiles/windirstat/WinDirStatPortable.exe', shell=True))
-		PageTwoButton6 = tk.Button(self, width=20, height=5, text="Backup and\nRestore Drivers", command=lambda: subprocess.Popen('%CD%/ProgFiles/doubledriver/dd.exe', shell=True))
+		PageTwoButton4 = tk.Button(self, width=20, height=5, text="Bulk Uninstall", command=lambda: subprocess.Popen('"%CD%/ProgFiles/myuninst/myuninst.exe"', shell=True))
+		PageTwoButton5 = tk.Button(self, width=20, height=5, text="Windows Directory\nStatistics", command=lambda: subprocess.Popen('"%CD%/ProgFiles/windirstat/WinDirStatPortable.exe"', shell=True))
+		PageTwoButton6 = tk.Button(self, width=20, height=5, text="Backup and\nRestore Drivers", command=lambda: subprocess.Popen('"%CD%/ProgFiles/doubledriver/dd.exe"', shell=True))
 
-		PageTwoButton7 = tk.Button(self, width=20, height=5, text="Tweaking's\n Windows Repair", command=lambda: subprocess.Popen('%CD%/ProgFiles/tweaking/Repair_Windows.exe', shell=True))
-		PageTwoButton8 = tk.Button(self, width=20, height=5, text="Rkill (Silent, Wait for Log)", command=lambda: subprocess.Popen('%CD%/ProgFiles/rkill.exe', shell=True))
-		PageTwoButton9 = tk.Button(self, width=20, height=5, text="Rootkit Scanner", command=lambda: subprocess.Popen('%CD%/ProgFiles/aswMBR.exe', shell=True))
+		PageTwoButton7 = tk.Button(self, width=20, height=5, text="Tweaking's\n Windows Repair", command=lambda: subprocess.Popen('"%CD%/ProgFiles/tweaking/Repair_Windows.exe"', shell=True))
+		PageTwoButton8 = tk.Button(self, width=20, height=5, text="Rkill (Silent, Wait for Log)", command=lambda: subprocess.Popen('"%CD%/ProgFiles/rkill.exe"', shell=True))
+		PageTwoButton9 = tk.Button(self, width=20, height=5, text="Rootkit Scanner", command=lambda: subprocess.Popen('"%CD%/ProgFiles/aswMBR.exe"', shell=True))
 
 		BackButton0.grid(row=3, column=2, padx=10, pady=10)
 
@@ -114,17 +114,17 @@ class HardwareTester(tk.Frame):
 
 		BackButton0 = tk.Button(self, width=20, height = 5, text="Go Back", command=lambda: controller.show_frame(StartPage))
 
-		PageThreeButton1 = tk.Button(self, width=20, height=5, text="Battery Health", command=lambda: subprocess.Popen('%CD%/ProgFiles/batteryinfoview/BatteryInfoView.exe', shell=True))
-		PageThreeButton2 = tk.Button(self, width=20, height=5, text="Keyboard", command=lambda: subprocess.Popen('', shell=True))
-		PageThreeButton3 = tk.Button(self, width=20, height=5, text="WebCam", command=lambda: subprocess.Popen('%CD%/ProgFiles/Camera.exe', shell=True))
+		PageThreeButton1 = tk.Button(self, width=20, height=5, text="Battery Health", command=lambda: subprocess.Popen('"%CD%/ProgFiles/batteryinfoview/BatteryInfoView.exe"', shell=True))
+		PageThreeButton2 = tk.Button(self, width=20, height=5, text="Keyboard", command=lambda: subprocess.Popen('""', shell=True))
+		PageThreeButton3 = tk.Button(self, width=20, height=5, text="WebCam", command=lambda: subprocess.Popen('"%CD%/ProgFiles/Camera.exe"', shell=True))
 
-		PageThreeButton4 = tk.Button(self, width=20, height=5, text="Left Speaker", command=lambda: subprocess.Popen('', shell=True))
-		PageThreeButton5 = tk.Button(self, width=20, height=5, text="Stress Tester", command=lambda: subprocess.Popen('', shell=True))
-		PageThreeButton6 = tk.Button(self, width=20, height=5, text="Right Speaker", command=lambda: subprocess.Popen('', shell=True))
+		PageThreeButton4 = tk.Button(self, width=20, height=5, text="Left Speaker", command=lambda: subprocess.Popen('""', shell=True))
+		PageThreeButton5 = tk.Button(self, width=20, height=5, text="Stress Tester", command=lambda: subprocess.Popen('""', shell=True))
+		PageThreeButton6 = tk.Button(self, width=20, height=5, text="Right Speaker", command=lambda: subprocess.Popen('""', shell=True))
 
-		PageThreeButton7 = tk.Button(self, width=20, height=5, text="WiFi Import/Export", command=lambda: subprocess.Popen('', shell=True))
-		PageThreeButton8 = tk.Button(self, width=20, height=5, text="Shutdown into\nRefresh or Reset", command=lambda: subprocess.Popen('shutdown -r -t 1 -f', shell=True))
-		PageThreeButton9 = tk.Button(self, width=20, height=5, text="Activation", command=lambda: subprocess.Popen('slmgr /xpr', shell=True))
+		PageThreeButton7 = tk.Button(self, width=20, height=5, text="WiFi Import/Export", command=lambda: subprocess.Popen('""', shell=True))
+		PageThreeButton8 = tk.Button(self, width=20, height=5, text="Shutdown into\nRefresh or Reset", command=lambda: subprocess.Popen('"shutdown -r -t 1 -f"', shell=True))
+		PageThreeButton9 = tk.Button(self, width=20, height=5, text="Activation", command=lambda: subprocess.Popen('"slmgr /xpr"', shell=True))
 
 		# USB EJECT
 		# CHECK DRIVERS
